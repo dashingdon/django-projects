6f013aefc6726f2789b4ec367d17a811e7f4a6dc
Tech User <Tech User>
2021-05-02 02:56:06 -0700

    progress

--- weather/src/weatherapp/views.py
+++ weather/src/weatherapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from config.settings import TEMPLATES
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Weather for the address")
-    return render(request,'index.html',{})
+    #template = loader.get_template('weatherapp/index.html')
+    return render(request,'first.html',{})
 
+def contact(request):
+    #return HttpResponse("Weather for the address")
+    if request.method == "POST":
+        cap_name = request.POST['name']
+        cap_phone = request.POST['phone']
+        print(cap_phone)
+        return render(request, 'contact.html', {'cap_phone': cap_phone})
+    else:
+        return render(request,'contact.html',{})
